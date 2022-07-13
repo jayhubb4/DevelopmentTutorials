34e414f3013534349499585ce2c9f4897e40a5cb
jayhubb4 <j.hubbard@softwearinc.com>
2022-07-13 16:12:46 -0400

    No fucking clue.

--- ReactWebsite-App/src/index.js
+++ ReactWebsite-App/src/index.js
@@ -1,6 +1,24 @@
-import ReactDOM from 'react-dom'
-import App from './App'
+
+/*import App from './App'
 import './style.css'
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+ReactDOM.render(<App/>, document.querySelector("#root"));
+*/
+import React from 'react';
+import App from './App';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import { createRoot } from 'react-dom/client';
 
+ReactDOM.createRoot(document.querySelector('root'))
+
+
+const rootElement = document.querySelector('root');
+const root = createRoot(rootElement); 
+
+
+root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
