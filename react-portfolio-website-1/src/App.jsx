2b1bec61346f15824e1c13a9037fe00438b0b68a
Jonathan Hubbard <gsusplaya@gmail.com>
2022-07-12 23:34:17 -0400

    Stopping point with problems. No visible text

--- react-portfolio-website-1/src/App.jsx
+++ react-portfolio-website-1/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
-const app = () => {
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/Portfolio/Portfolio'
+import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+const App = () => {
   return (
-    <div>app</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
-export default app
+export default App
