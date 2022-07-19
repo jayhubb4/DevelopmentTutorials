ec70ab28039780cf80f56056fef872b87d42bd97
Jonathan Hubbard <gsusplaya@gmail.com>
2022-07-18 23:40:49 -0400

    EON commit. Bottom image needs styling

--- another-app-trial/src/containers/possibility/Possibility.jsx
+++ another-app-trial/src/containers/possibility/Possibility.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Possibility.css'
+import possibilityimage from '../../assets/possibilty.png'
 
 const Possibiility = () => {
   return (
-    <div>
-        Possibiility
-
-
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityimage} alt='possibilty'></img>
+      </div>
+      <div className='gpt3__possibilty-content'>
+        <h4>Request Early Access to get Started</h4>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Can you see yourself in/with this? We can. This is now. Let this furture be a part of your past</p>
+        <h4>Request Early Access to get Started</h4>
+      </div>
     </div>
   )
 }
