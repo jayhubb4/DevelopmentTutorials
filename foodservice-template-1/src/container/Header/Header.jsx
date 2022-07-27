f0d4f253db2eb4c8dcd272099a6d6b09b2acecb7
Jonathan Hubbard <gsusplaya@gmail.com>
2022-07-26 22:54:18 -0400

    Special Menu styling needed.

--- foodservice-template-1/src/container/Header/Header.jsx
+++ foodservice-template-1/src/container/Header/Header.jsx
@@ -10,11 +10,11 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title='Chase the New Flavor' />
       <h1 className='app__header-h1'>The Key to Fine Dining</h1>
-      <p className='p__opesans' style={{ margin: '2rem 0'}}> Sit tellus sed senectus vivamus moelstie. Condementum volupat marbi facilis quam scelerisque sapien. Et, penatbius aliquam amet tellus</p>
+      <p className='p__opensans' style={{ margin: '2rem 0'}}> Sit tellus sed senectus vivamus moelstie. Condementum volupat marbi facilis quam scelerisque sapien. Et, penatbius aliquam amet tellus</p>
       <button type='button' className='custom__button'>Explore Menu</button>
     </div>
 
-    <div className='app_wrapper_img'>
+    <div className='app__wrapper_img'>
       <img src= {images.welcome} alt='header img' /> 
 
     </div>
