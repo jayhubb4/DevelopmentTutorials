06c5d03eaedddb5b6cfebd8d66e265e0a1c98d7a
Jonathan Hubbard <gsusplaya@gmail.com>
2022-07-27 23:01:55 -0400

    IDK WTF

--- foodservice-template-1/src/container/Menu/SpecialMenu.jsx
+++ foodservice-template-1/src/container/Menu/SpecialMenu.jsx
@@ -13,9 +13,9 @@ const SpecialMenu = () => (
     <div className='app__specialMenu-menu'>
       <div className='app___specialMenu-menu_wine flex__center'>
         <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
-        <div className='app__specialMenu-menu_items'>
+        <div className='app__specialMenu_menu_items'>
           {data.wines.map((wine, index) => (
-            <p>{wine.title}</p>
+            <MenuItem key={wine.title+index} title={wine.title} price={wine.price} tags={wine.tags} />
           ))}
         </div>
       </div>
@@ -25,10 +25,10 @@ const SpecialMenu = () => (
       </div>
 
       <div className='app___specialMenu-menu_cocktails flex__center'>
-        <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
-        <div className='app__specialMenu-menu_items'>
+        <p className='app__specialMenu-menu_heading'>Cocktails</p>
+        <div className='app__specialMenu_menu_items'>
           {data.cocktails.map((cocktail, index) => (
-            <p>{cocktail.title}</p>
+            <MenuItem key={cocktail.title+index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
           ))}
         </div>
       </div>
