afaffe1582d5e0abe01fd251934963ae506df943
Jonathan Hubbard <gsusplaya@gmail.com>
2022-07-28 23:02:28 -0400

    Adding video. there are errors

--- foodservice-template-1/src/container/Intro/Intro.jsx
+++ foodservice-template-1/src/container/Intro/Intro.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BaFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const vidRef = React.useRef ();
+  useState
+  return {
+    <div className='app__video'
+      <video 
+      src={meal}
+      ref={vidRef}
+      type="video/mp4"
+      Loop
+      controls={false}
+      muted
+      />
+      
+    </div>
+ }
+}
 
 export default Intro;
