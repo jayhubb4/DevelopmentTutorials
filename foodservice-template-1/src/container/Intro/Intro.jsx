ebb2eadea848c8f5d3faab161cf29a3117daf66c
jayhubb4 <j.hubbard@softwearinc.com>
2022-07-29 14:26:02 -0400

    Small changes to intro

--- foodservice-template-1/src/container/Intro/Intro.jsx
+++ foodservice-template-1/src/container/Intro/Intro.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
-import { BaFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import { meal } from '../../constants';
 
 import './Intro.css';
 
+
 const Intro = () => {
-  const vidRef = React.useRef ();
-  useState
-  return {
-    <div className='app__video'
+  const [state, setState] = useState(initialState)
+  const vidRef = React.useRef();
+  
+  return (
+    <div className='app__video'>
       <video 
-      src={meal}
-      ref={vidRef}
-      type="video/mp4"
-      Loop
-      controls={false}
-      muted
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        Loop
+        controls={false}
+        muted
       />
       
     </div>
- }
+  )
 }
 
 export default Intro;
