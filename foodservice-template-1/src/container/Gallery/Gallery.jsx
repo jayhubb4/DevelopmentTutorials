5cf85fe917dd4dc71474b8dc00043c4d469f8a45
Jonathan Hubbard <gsusplaya@gmail.com>
2022-07-30 22:45:01 -0400

    Hot fix

--- foodservice-template-1/src/container/Gallery/Gallery.jsx
+++ foodservice-template-1/src/container/Gallery/Gallery.jsx
@@ -33,6 +33,7 @@ return (
       <div className='app__gallery-images'>
         <div className='app__gallery-images_container' ref={scrollRef}>
           {galleryimages.map((image, index) => (
+            // eslint-disable-next-line no-template-curly-in-string
             <div className='app__gallery-images_card flex__center' key={'gallery_image-${index + 1}'}>
               <img src={image} alt="gallery" />
               <BsInstagram className='gallery__image-icon'/>
