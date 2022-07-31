a354e3945a56cc8803a2df03f64d5c3555e43020
Jonathan Hubbard <gsusplaya@gmail.com>
2022-07-30 22:39:14 -0400

    Finished gallery, intro, and laurels. 40m to go

--- foodservice-template-1/src/container/Intro/Intro.jsx
+++ foodservice-template-1/src/container/Intro/Intro.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import { meal } from '../../constants';
 
@@ -6,9 +6,18 @@ import './Intro.css';
 
 
 const Intro = () => {
-  const [state, setState] = useState(initialState)
-  const vidRef = React.useRef();
+  const [playVideo, setplayVideo] = React.useState(false);
+  const vidRef = React.useRef ();
+  const handleVideo = () => {
+    setplayVideo((prevPlayVideo) => !prevPlayVideo) //!prevPlayVideo means NOT previous play video. If play video is true we'll toggle it off if it's false we'll toggle it on. 
+    
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
   
+  }
   return (
     <div className='app__video'>
       <video 
@@ -20,7 +29,19 @@ const Intro = () => {
         muted
       />
       
+    <div className='app__video-overlay flex__center'>
+      <div 
+      className='app__video-overlay_circle flex__center'
+      onClick={handleVideo}
+      >
+        {playVideo 
+        ? <BsPauseFill color='#fff' fontSize={30} />
+        
+        : <BsFillPlayFill color='#fff' fontSize={30} />}
+
+      </div>
     </div>
+  </div>
   )
 }
 
