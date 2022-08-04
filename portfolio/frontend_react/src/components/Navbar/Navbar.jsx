b1eb2c047500413637f4f7b6b9c3cb74a1a09a0a
Jonathan Hubbard <gsusplaya@gmail.com>
2022-08-03 23:53:00 -0400

    Fixed sanity. Portfolio started

---
+++ portfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Navbar.scss';
+import { images } from '../../constants';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
