927c594ac3522524b5098673290514a362661f9f
Jonathan Hubbard <gsusplaya@gmail.com>
2022-08-08 23:53:22 -0400

    Needs work

--- portfolio/frontend_react/src/components/Navbar/Navbar.jsx
+++ portfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -23,13 +23,11 @@ const Navbar = () => {
       </ul>
 
       <div className='app__navbar-menu'>
-          <HiMenuAlt4 onClick={() => setToggle(true)} />
-
-          {
-            toggle && (
+          <HiMenuAlt4 onClick={() => setToggle(true)} />         
+            {toggle && (
               <motion.div
               whileInView={{ x: [300, 0] }}
-              transition={{ duration: 0.85, ease: "easeOut" }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}
               >
                 <HiX onClick={() => setToggle(false)} />
                 {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
