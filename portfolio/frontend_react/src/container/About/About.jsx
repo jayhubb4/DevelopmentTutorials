5871a5b3f568d355cc97eadc7699735bb5517906
Jonathan Hubbard <gsusplaya@gmail.com>
2022-08-12 18:37:13 -0400

    Corrected my error. Testimonial section next

--- portfolio/frontend_react/src/container/About/About.jsx
+++ portfolio/frontend_react/src/container/About/About.jsx
@@ -3,7 +3,7 @@ import './About.scss';
 import { motion } from 'framer-motion';
 
 import { urlFor, client } from '../../client';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 
 const About = () => {  
   const [abouts, setAbouts] = useState([]);
@@ -41,4 +41,8 @@ const About = () => {
   )
 }
 
-export default AppWrap(About, 'about');
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  "app__whitebg",
+  );
