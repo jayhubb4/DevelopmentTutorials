a74597987ae9f93604fa37b892afc8a7e943b0a8
Jonathan Hubbard <gsusplaya@gmail.com>
2022-08-18 22:53:17 -0400

    86'd body builder app. Starting ecommerce app

--- gym-app/src/App.js
+++ gym-app/src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
-import ExerciseDetail from './pages/ExerciseDetail/';
-import Home from './pages/Home/';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-const App = () => {
-  return (
+const App = () => (
     <Box 
     width='400px'
     sx={{ width: { xl: '1488px' }}} m="auto"
@@ -21,7 +20,6 @@ const App = () => {
         <Footer />
 
     </Box>
-  )
-}
+);
 
-export default App
+export default App;
