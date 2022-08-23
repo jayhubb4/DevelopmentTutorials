c016dd1ebda2150693ae36f6bd21133e26e16f51
Jonathan Hubbard <gsusplaya@gmail.com>
2022-08-22 23:16:36 -0400

    On to add product details

--- ecommerce/ecomm/pages/index.js
+++ ecommerce/ecomm/pages/index.js
@@ -5,19 +5,19 @@ import { client } from '../lib/client';
 const Home = ({ products, bannerData }) => (
 
     <div>
-      <HeroBanner />
-        {console.log(bannerData)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+        
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className='products-container'>
-        {products?.map((product) => product.name
+        {products?.map((product) => <Product  key={product._id} product={product}/>
         )}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </div>
 );
 
