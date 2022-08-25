31bcade04f6f0d739e4b25b751cf9f993144d8fa
Jonathan Hubbard <gsusplaya@gmail.com>
2022-08-25 00:17:48 -0400

    Ready for cart logic

--- ecommerce/ecomm/pages/product/[slug].js
+++ ecommerce/ecomm/pages/product/[slug].js
@@ -9,7 +9,7 @@ import { useStateContext } from '../../context/StateContext';
 const ProductDetails = ({ product, products }) => {
 const { image, name, details, price } = product;
 const [index, setIndex] = useState(0);
-const { decQty, incQty, qty } = useStateContext();
+const { decQty, incQty, qty, onAdd } = useStateContext();
   return (
     <div>
         <div
@@ -69,7 +69,7 @@ const { decQty, incQty, qty } = useStateContext();
                     <button 
                     type='button' 
                     className='add-to-cart'
-                    onClick=''>Add to Cart
+                    onClick={() => onAdd(product, qty)}>Add to Cart
                     </button>
                     <button 
                     type='button' 
