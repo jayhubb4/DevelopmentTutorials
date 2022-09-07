0d4069e7bcfb40b15908f9b6f2e298e15c75ea83
Jonathan Hubbard <gsusplaya@gmail.com>
2022-09-06 22:21:03 -0400

    changed header in ecommerce

--- ecommerce/ecomm/components/Navbar.jsx
+++ ecommerce/ecomm/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div className='navbar-container'>
       <p className='logo'>
         <Link href='/'>
-          CodeScaping Electronics *Ecommerce Example Webpage*
+          .com Electronics *Ecommerce Example Website*
         </Link>
       </p>
       <button
