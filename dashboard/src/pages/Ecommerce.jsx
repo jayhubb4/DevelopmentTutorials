d97764aab7157d2bd518b0d7cd7cf7308f1f1a9a
Jonathan Hubbard <gsusplaya@gmail.com>
2022-11-22 00:17:05 -0500

    lots to finish

--- dashboard/src/pages/Ecommerce.jsx
+++ dashboard/src/pages/Ecommerce.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BsCurrencyDollar } from 'react-icons/bs';
 import { GoPrimitiveDot } from 'react-icons/go';
 import { Stacked, Pie, Button, SparkLine} from '../components';
-import { earningData, SparkLineAreaData, ecomPieChartData } from '../data/dummy';
+import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../context/ContextProvider';
 
 
@@ -10,7 +10,7 @@ const Ecommerce = () => {
   return (
     <div className='mt-12'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
-        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
           <div className='flex justify-between items-center'>
             <div>
               <p className='font-bold text-gray-400'>Earnings</p>
@@ -96,7 +96,7 @@ const Ecommerce = () => {
                     type='Line'
                     height='80px'
                     width='250px'
-                    data={SparkLineAreaData} 
+                    data={SparklineAreaData} 
                     color='blue'
                     />
                   </div>
