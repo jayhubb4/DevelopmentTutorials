7ca3123bc88206f73e25e78d52f007a38cf35c7b
Jonathan Hubbard <gsusplaya@gmail.com>
2022-11-22 22:44:34 -0500

    Calendar and cutomers done

--- dashboard/src/pages/Customers.jsx
+++ dashboard/src/pages/Customers.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
-const Customer = () => {
+const Customers = () => {
   return (
-    <div>Customer</div>
+    <div
+      className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header
+        title='Customers'
+        category='Page' />
+      <GridComponent
+        width='auto'
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        toolbar={['Delete']}
+        >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[ Page, Toolbar, Selection, Edit, Sort, Filter ]} />
+          
+      </GridComponent>
+  </div>
   )
 }
 
-export default Customer
+export default Customers
