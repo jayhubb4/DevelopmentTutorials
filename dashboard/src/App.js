5f6767d4e1d42a059c2d1b9a31716e93b6a58cac
Jonathan Hubbard <gsusplaya@gmail.com>
2022-12-02 22:25:06 -0500

    EOD

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -45,6 +45,8 @@ const App = () => {
           </div>
 
           <div>
+            <ThemeSettings />
+            
             <Routes>
               {/* Dashboard */}
               <Route path='/' element={<Ecommerce/>} />
