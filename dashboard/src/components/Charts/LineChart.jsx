fc3f163c8f6b40beed78b55aeddd7f5ee887630a
jayhubb4 <j.hubbard@softwearinc.com>
2022-12-06 17:15:35 -0500

    Finish at home tonight

--- dashboard/src/components/Charts/LineChart.jsx
+++ dashboard/src/components/Charts/LineChart.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectiveDirective, SeriesDirective, Inject, DateTime, LineSeries, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis} from '../../data/dummy';
 
 const LineChart = () => {
   return (
-    <div>LineChart</div>
+    <ChartComponent>
+      <Inject 
+      services={[ LineSeries, DateTime, Legend, Tooltip ]} />
+    </ChartComponent>
   )
 }
 
