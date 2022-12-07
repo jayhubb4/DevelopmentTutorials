83561487a896772a8f5249aecaf49e8b11434953
Jonathan Hubbard <gsusplaya@gmail.com>
2022-12-06 22:56:48 -0500

    Finished

--- dashboard/src/components/Charts/LineChart.jsx
+++ dashboard/src/components/Charts/LineChart.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { ChartComponent, SeriesCollectiveDirective, SeriesDirective, Inject, DateTime, LineSeries, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, LineSeries, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
 import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis} from '../../data/dummy';
+import { useStateContext } from '../../context/ContextProvider';
 
 const LineChart = () => {
+  const { currentMode } = useStateContext();
+  
   return (
-    <ChartComponent>
+    <ChartComponent
+    id='line-chart'
+    height='420px'
+    primaryXAxis={LinePrimaryXAxis}
+    primaryYAxis={LinePrimaryYAxis}
+    chartArea={{ border: { width: 0 }}}
+    tooltip={{ enable: true }}
+    background={currentMode === 'Dark' ? '#33373E' : '#FFF'}
+    >
       <Inject 
-      services={[ LineSeries, DateTime, Legend, Tooltip ]} />
+        services={[ LineSeries, DateTime, Legend, Tooltip ]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
     </ChartComponent>
   )
 }
--- dashboard/src/data/dummy.js
+++ dashboard/src/data/dummy.js
@@ -5865,7 +5865,7 @@ export const financialChartData = [
 ];
 export const PyramidData = [
   { x: 'Sweet Treats', y: 120, text: '120 cal' },
-  { x: 'Milk, Youghnut, Cheese', y: 435, text: '435 cal' },
+  { x: 'Milk, Doughnut, Cheese', y: 435, text: '435 cal' },
   { x: 'Vegetables', y: 470, text: '470 cal' },
   { x: 'Meat, Poultry, Fish', y: 475, text: '475 cal' },
   { x: 'Fruits', y: 520, text: '520 cal' },
