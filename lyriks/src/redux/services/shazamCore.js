be11462d34a58cb1899145c0d0b5c9b6a755a100
Jonathan Hubbard <gsusplaya@gmail.com>
2022-12-15 00:14:16 -0500

    Night

---
+++ lyriks/src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createAPI, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createAPI({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '531543a417msha86c761b31286e6p17e37bjsn8187a242a269');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
