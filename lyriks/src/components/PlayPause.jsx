85a22ad16d6654aa5189dbe764172b002dc3ae5d
jayhubb4 <j.hubbard@softwearinc.com>
2022-12-15 16:56:53 -0500

    Do more tonight

--- lyriks/src/components/PlayPause.jsx
+++ lyriks/src/components/PlayPause.jsx
@@ -1,5 +1,17 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCirlce, FaPlayCircle } from 'react-icons/fa';
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (isPlaying && activeSong?.title === song.title ? (
+  <FaPauseCirlce 
+    size={35}
+    classname='text-gray-300'
+    onClick={handlePause}
+  />
+) : (
+  <FaPlayCircle
+    size={35}
+    classname='text-gray-300'
+    onClick={handlePlay}
+  />
+));
 
 export default PlayPause;
