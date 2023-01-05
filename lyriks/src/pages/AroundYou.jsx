d6aa92bf17bb08e8c6a9b966ee4a1d5153a29660
Jonathan Hubbard <gsusplaya@gmail.com>
2023-01-05 00:15:15 -0500

    done but broken

--- lyriks/src/pages/AroundYou.jsx
+++ lyriks/src/pages/AroundYou.jsx
@@ -7,47 +7,43 @@ import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 const AroundYou = () => {
   const [country, setCountry] = useState('');
   const [loading, setLoading] = useState(true);
-  const { activeSong,  isPlaying } = useSelector((state) => state.player);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
-
-  if(isFetching && loading) return <Loader title="Finding songs around you..." />;
-
-  if(error && country) return <Error />;
 
   useEffect(() => {
     // at_WAp0lgzj1ntrA0Kqhpufw92uMp86J
-    axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_WAp0lgzj1ntrA0Kqhpufw92uMp86J&`)
+    axios
+      .get('https://geo.ipify.org/api/v2/country?apiKey=at_WAp0lgzj1ntrA0Kqhpufw92uMp86J&ipAddress=8.8.8.8')
       .then((res) => setCountry(res?.data?.location?.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
-
-
   }, [country]);
 
-return (
-   <div className="flex flex-col">
-    <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+  if (isFetching && loading) return <Loader title="Finding songs around you..." />;
+
+  if (error && country !== '') return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
         Around You
-          <span className="font-black">{country}</span>
-        </h2>
-    
-    <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-      {data?.map((song, i) => (
-        <SongCard
-          key={song.key}
-          song={song}
-          isPlaying={isPlaying}
-          activeSong={activeSong}
-          data={data}
-          i={i}
-        />
-      ))}
+        <span className="font-black">{country}</span>
+      </h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
     </div>
-   </div>
-
-
-);
+  );
 };
-
 
 export default AroundYou;
