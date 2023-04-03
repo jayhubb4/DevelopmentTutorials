2f793d09a14800171ced9f78af9cefb90c91a83e
Jonathan Hubbard <gsusplaya@gmail.com>
2023-04-03 00:06:46 -0400

    Just some cleaning and training

---
+++ FlexboxTutorial/flexboxtut/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Page from './Page';
+
+function App() {
+  return (
+    <div className="App">
+      <Page />
+    </div>
+  );
+}
+
+export default App;
---
+++ FlexboxTutorial/flexboxtut/src/Page.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+const Page = () => {
+  return (
+    <div className='image-container'>
+        <img src='./images/img-1.jpg' alt='img' />
+        <img src='./images/img-2.jpg' alt='img' />
+        <img src='./images/img-3.jpg' alt='img' />
+        <img src='./images/img-4.jpg' alt='img' />
+        <img src='./images/img-5.jpg' alt='img' />
+        <img src='./images/img-6.jpg' alt='img' />
+        <img src='./images/img-7.jpg' alt='img' />
+        <img src='./images/img-8.jpg' alt='img' />
+        <img src='./images/img-9.jpg' alt='img' />
+    </div>
+
+  )
+}
+
+export default Page
