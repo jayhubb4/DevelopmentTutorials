27a2dfa8e19d02aaa3aab883dbfe5c5ac48f16c1
Jonathan Hubbard <gsusplaya@gmail.com>
2023-04-04 18:16:33 -0400

    just some training and bug fixes

--- FlexboxTutorial/flexboxtut/src/Page.jsx
+++ FlexboxTutorial/flexboxtut/src/Page.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
+import { images } from './images';
 
 const Page = () => {
   return (
-    <div className='image-container'>
-        <img src='./images/img-1.jpg' alt='img' />
-        <img src='./images/img-2.jpg' alt='img' />
-        <img src='./images/img-3.jpg' alt='img' />
-        <img src='./images/img-4.jpg' alt='img' />
-        <img src='./images/img-5.jpg' alt='img' />
-        <img src='./images/img-6.jpg' alt='img' />
-        <img src='./images/img-7.jpg' alt='img' />
-        <img src='./images/img-8.jpg' alt='img' />
-        <img src='./images/img-9.jpg' alt='img' />
+    
+    <div className='app2'>
+      <div className='container'>
+        <div className='images'>
+            <img src={images.img1} alt='img' />
+            <img src={images.img2} alt='img' />
+            <img src={images.img3} alt='img' />
+            <img src={images.img4} alt='img' />
+            <img src={images.img5} alt='img' />
+            <img src={images.img6} alt='img' />
+            <img src={images.img7} alt='img' />
+            <img src={images.img8} alt='img' />
+            <img src={images.img9} alt='img' />
+        </div>
+      </div>
     </div>
-
   )
 }
 
